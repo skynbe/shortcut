bc18ec7abc46021a65c748fecc5ae7e686b753d9
skynbe <skynbe@gmail.com>
2018-07-21 03:27:41 +0900

    avgmeter fix

--- metric.py
+++ metric.py
@@ -10,14 +10,14 @@ class AvgMeter(object):
         self.reset()
 
     def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
+        self.val = 0.
+        self.avg = 0.
+        self.sum = 0.
+        self.count = 0.
 
     def update(self, val, n=1):
         self.val = val
-        self.sum += val * n
+        self.sum += val
         self.count += n
         self.avg = self.sum / self.count
 
